0ee442d38a38a1de95cd1227a885526fcb063e8e
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-04-18 20:14:02 +0530

    render template

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import socket
 
 hostname = socket.gethostname()
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
